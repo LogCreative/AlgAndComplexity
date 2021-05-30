7490983236a0f3667d6d0079ccc0fb5a283a8c7e
LogCreative <61653082+LogCreative@users.noreply.github.com>
2021-05-30 20:40:16 +0800

    Update filter.ps1

--- filter.ps1
+++ filter.ps1
@@ -7,6 +7,6 @@ Get-ChildItem . '*.tex' -Force -Recurse | ForEach-Object -Process {
         Out-File $file -Force
 }
 
-Get-ChildItem .\ -recurse *-ZilongLi* | ForEach-Object {
+Get-ChildItem .\ -recurse *-********** | ForEach-Object {
     Rename-Item $_.FullName$_.FullName.Replace("-********","")
 }
