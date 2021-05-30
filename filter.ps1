df47e3442910e84d2b0f2187647f83e11cd6d271
LogCreative <longlaoda123456@qq.com>
2021-05-30 18:01:13 +0800

    clean up

---
+++ filter.ps1
@@ -0,0 +1,12 @@
+Get-ChildItem . '*.tex' -Force -Recurse | ForEach-Object -Process {
+    $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
+
+    (Get-Content $file)|
+        ForEach-Object { $_ -replace '*******','Log Creative'} |  # Change Name
+        ForEach-Object { $_ -replace '*******',''} | # Change Number
+        Out-File $file -Force
+}
+
+Get-ChildItem .\ -recurse *-ZilongLi* | ForEach-Object {
+    Rename-Item $_.FullName$_.FullName.Replace("-ZilongLi","")
+}
